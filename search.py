812c9b48c8c1ba282bf441de9fa5a540cfa33c11
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2017-11-25 18:11:12 +0530

    minor change on url

--- search.py
+++ search.py
@@ -214,7 +214,7 @@ def getSecondaryName(primary,secondary):
 
 
 def getAmazonURL(name):
-    res = service.cse().list(q=name + " Phone ", cx="006500068614198421475:xhtaxqo-nbc").execute()
+    res = service.cse().list(q=name + " Phone dp", cx="006500068614198421475:xhtaxqo-nbc").execute()
     if int(res["searchInformation"]["totalResults"]) > 0:
         u = res["items"][0]["link"]
         print u
